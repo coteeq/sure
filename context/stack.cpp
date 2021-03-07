8aa612e55cff33b876d0b6fcf4b29919a8e6f6a4
Roman Lipovsky <roman.lipovsky@gmail.com>
2021-03-07 13:06:31 +0300

    comment

--- context/stack.cpp
+++ context/stack.cpp
@@ -12,6 +12,7 @@ Stack::Stack(MmapAllocation allocation) : allocation_(std::move(allocation)) {
 
 Stack Stack::AllocatePages(size_t count) {
   auto allocation = MmapAllocation::AllocatePages(count + 1);
+  // Guard page
   allocation.ProtectPages(/*offset=*/0, /*count=*/1);
   return Stack{std::move(allocation)};
 }
