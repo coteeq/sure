d2c350979068cd71f5cd7bf110d806bc980cfeec
Roman Lipovsky <roman.lipovsky@gmail.com>
2022-04-01 00:26:09 +0300

    consistent naming in cmake, do not use file

--- cmake/Arch.cmake
+++ cmake/Arch.cmake
@@ -1,7 +1,12 @@
-if((CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "aarch64") OR (CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "arm")) 
-    set(CTX_ARCH "armv8_a_64")
+message(STATUS "Host system processor: ${CMAKE_HOST_SYSTEM_PROCESSOR}")
+
+# Workaround: default
+set(CONTEXT_ARCH "x86_64")
+
+if((CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "aarch64") OR (CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "arm"))
+    set(CONTEXT_ARCH "armv8_a_64")
 endif()
 
 if(CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "x86_64")
-    set(CTX_ARCH "x86_64")
+    set(CONTEXT_ARCH "x86_64")
 endif()
