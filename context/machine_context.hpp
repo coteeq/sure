893d58fd330aa6953efcfcc94c864c3408ae6342
Roman Lipovsky <roman.lipovsky@gmail.com>
2022-04-08 01:55:10 +0300

    arch_ prefix for compile def

--- context/machine_context.hpp
+++ context/machine_context.hpp
@@ -1,9 +1,9 @@
 #pragma once
 
-#if defined(x86_64)
+#if defined(arch_x86_64)
     #include <context/arch/x86_64/machine_context.hpp>
 #endif // x86_64
 
-#if defined(armv8_a_64)
+#if defined(arch_armv8_a_64)
     #include <context/arch/armv8_a_64/machine_context.hpp>
 #endif // armv8_a_64
