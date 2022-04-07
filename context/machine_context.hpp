5752ae42102afb623e097a4e1a459afbb85c307f
Roman Lipovsky <roman.lipovsky@gmail.com>
2022-04-08 01:58:40 +0300

    elif

--- context/machine_context.hpp
+++ context/machine_context.hpp
@@ -2,8 +2,6 @@
 
 #if defined(arch_x86_64)
     #include <context/arch/x86_64/machine_context.hpp>
-#endif // x86_64
-
-#if defined(arch_armv8_a_64)
+#elif defined(arch_armv8_a_64)
     #include <context/arch/armv8_a_64/machine_context.hpp>
-#endif // armv8_a_64
+#endif
