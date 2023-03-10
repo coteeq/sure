afa8993607e979de4615077173b9e0bcc1ce1de5
Roman Lipovsky <roman.lipovsky@gmail.com>
2023-03-10 15:31:25 +0300

    cosmetics

--- cmake/Arch.cmake
+++ cmake/Arch.cmake
@@ -1,12 +1,12 @@
 ProjectLog("Host system processor: ${CMAKE_HOST_SYSTEM_PROCESSOR}")
 
 # Workaround: default
-set(CONTEXT_ARCH "x86_64")
+set(SURE_ARCH "x86_64")
 
 if((CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "aarch64") OR (CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "arm") OR (CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "arm64"))
-    set(CONTEXT_ARCH "armv8_a_64")
+    set(SURE_ARCH "armv8_a_64")
 endif()
 
 if(CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "x86_64")
-    set(CONTEXT_ARCH "x86_64")
+    set(SURE_ARCH "x86_64")
 endif()
--- sure/machine_context.hpp
+++ sure/machine_context.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-#if defined(arch_x86_64)
+#if defined(SURE_ARCH_x86_64)
     #include <sure/arch/x86_64/machine_context.hpp>
-#elif defined(arch_armv8_a_64)
+#elif defined(SURE_ARCH_armv8_a_64)
     #include <sure/arch/armv8_a_64/machine_context.hpp>
 #endif
